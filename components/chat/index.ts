cc43db55356f1e4c3c99dedd36d06abcfc49c990
ReRail <info.rerail@gmail.com>
2026-01-11 01:00:43 +0900

    fix: RecipeLoadingBubbleのエクスポートを削除

--- components/chat/index.ts
+++ components/chat/index.ts
@@ -1,7 +1,3 @@
 export { ChatMessageBubble, type ChatAttachment, type ChatMessageBubbleProps } from './ChatMessageBubble';
 export { QuickReplyChips, type QuickReply } from './QuickReplyChips';
 export { ComposerBar } from './ComposerBar';
-export { RecipeLoadingBubble } from './RecipeLoadingBubble';
-
-
-
